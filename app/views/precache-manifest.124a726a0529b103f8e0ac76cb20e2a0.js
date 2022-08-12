ebc3e3c77715e71d3b6d3a02303f9f00934c6760
Camille <c.hughes64@gmail.com>
2022-08-12 12:35:56 -0400

    connecting vue and express / build files

---
+++ app/views/precache-manifest.124a726a0529b103f8e0ac76cb20e2a0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fad40bf665d9c407be",
+    "url": "/css/app.3b72bb5a.css"
+  },
+  {
+    "revision": "e2854e7dcf4ff4fc6284",
+    "url": "/css/chunk-28c2dbda.1ac21ac2.css"
+  },
+  {
+    "revision": "60f35fe7eaa66dafbbcf",
+    "url": "/css/chunk-65ed2158.a679a1ea.css"
+  },
+  {
+    "revision": "d11484d44c66eb30fcc43ddfcbdfe018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fad40bf665d9c407be",
+    "url": "/js/app.7355ec3c.js"
+  },
+  {
+    "revision": "e2854e7dcf4ff4fc6284",
+    "url": "/js/chunk-28c2dbda.09d57c58.js"
+  },
+  {
+    "revision": "40aea0f30e66ef0afbe1",
+    "url": "/js/chunk-2d22c4a8.31261617.js"
+  },
+  {
+    "revision": "60f35fe7eaa66dafbbcf",
+    "url": "/js/chunk-65ed2158.2dbec70d.js"
+  },
+  {
+    "revision": "c7839ca926e46b9e39de",
+    "url": "/js/chunk-ae72dd88.a40ed166.js"
+  },
+  {
+    "revision": "c96cba0d28d2637f585e",
+    "url": "/js/chunk-vendors.15d0d28c.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

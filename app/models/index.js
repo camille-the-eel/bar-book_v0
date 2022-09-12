6eb942dc50b1223938774e139d06ca54689c7242
Camille <c.hughes64@gmail.com>
2022-09-12 16:39:30 -0400

    jawsdb config update

--- app/models/index.js
+++ app/models/index.js
@@ -24,8 +24,11 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     config.database,

e4a3af97717ec38f5c5ec8d72381d6ec6573fe6c
Camille <c.hughes64@gmail.com>
2022-09-20 13:55:34 -0400

    updated build files

---
+++ app/views/precache-manifest.502819ba501f04457da3fb394b355287.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e4395b8d19fe0aa4f0",
+    "url": "/css/app.a49f34ad.css"
+  },
+  {
+    "revision": "972bc6ad7665a62cec66",
+    "url": "/css/chunk-7692a0ca.01af9c7c.css"
+  },
+  {
+    "revision": "7b94b161f54ec26927c9",
+    "url": "/css/chunk-e4331ff0.a679a1ea.css"
+  },
+  {
+    "revision": "14ba06301db1032c2ad569ed4914c7d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e4395b8d19fe0aa4f0",
+    "url": "/js/app.f7a6d7cc.js"
+  },
+  {
+    "revision": "a02d2017ba6b2871ad71",
+    "url": "/js/chunk-2d22c4a8.5446c1ef.js"
+  },
+  {
+    "revision": "972bc6ad7665a62cec66",
+    "url": "/js/chunk-7692a0ca.bbdfe8c4.js"
+  },
+  {
+    "revision": "89dc155d9c8679130bd6",
+    "url": "/js/chunk-ae72dd88.76e5f13f.js"
+  },
+  {
+    "revision": "7b94b161f54ec26927c9",
+    "url": "/js/chunk-e4331ff0.172d3f93.js"
+  },
+  {
+    "revision": "4fe295e07887f75b1bc8",
+    "url": "/js/chunk-vendors.ed2bd3c2.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

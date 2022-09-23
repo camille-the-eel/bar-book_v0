5e1fc9ad50b47ea73d524a14074091e4082ff593
Camille <c.hughes64@gmail.com>
2022-09-23 16:10:11 -0400

    updated build files | tables css

---
+++ app/views/precache-manifest.a6b0596cca851bcbe7c054798fa5c1fd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5449f33ee6a83d0a5f",
+    "url": "/css/app.bd031a30.css"
+  },
+  {
+    "revision": "567e0cd81f73cec9c3f5",
+    "url": "/css/chunk-3c5451d8.e2c0b931.css"
+  },
+  {
+    "revision": "e4deb84516d80b759b0a",
+    "url": "/css/chunk-64b3a113.429c05f7.css"
+  },
+  {
+    "revision": "13a57df9b79865f6955bb73a09041d0b",
+    "url": "/img/lbb_chartreuse_1.13a57df9.png"
+  },
+  {
+    "revision": "2cd4dffef477b9aa9a2deb53cd6352d6",
+    "url": "/img/portrait_filled.2cd4dffe.svg"
+  },
+  {
+    "revision": "136dadaa637ac0e47819705fddc2ad86",
+    "url": "/img/search.136dadaa.svg"
+  },
+  {
+    "revision": "cbf69c7a2b6b44fd588778314237fa71",
+    "url": "/img/stars_filled.cbf69c7a.svg"
+  },
+  {
+    "revision": "895719d495fcd31a53a7e52cf4751266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e5449f33ee6a83d0a5f",
+    "url": "/js/app.54f246ef.js"
+  },
+  {
+    "revision": "6a7b69de8007e2566ea5",
+    "url": "/js/chunk-2c38fd92.7c32fdf7.js"
+  },
+  {
+    "revision": "567e0cd81f73cec9c3f5",
+    "url": "/js/chunk-3c5451d8.feeeba0f.js"
+  },
+  {
+    "revision": "35e5ae4708f2ec3935a1",
+    "url": "/js/chunk-5047e614.2a95ab4e.js"
+  },
+  {
+    "revision": "e4deb84516d80b759b0a",
+    "url": "/js/chunk-64b3a113.ec48ebb4.js"
+  },
+  {
+    "revision": "cc610484410dbaf66932",
+    "url": "/js/chunk-vendors.92ec7841.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
